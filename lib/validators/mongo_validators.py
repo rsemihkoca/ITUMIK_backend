68a118349100a2ba10b7491d637aa0005a3e45b0
Riza Semih Koca <rsemihkoca@outlook.com>
2023-06-10 19:22:34 +0300

    commit

--- lib/validators/mongo_validators.py
+++ lib/validators/mongo_validators.py
@@ -4,10 +4,12 @@ logger = logging.getLogger()  # Retrieve the root logger
 
 def check_mongodb_parameters():
     # check if .env variables are defined
-    if Configs.DB_USERNAME is None:
-        raise Exception("DB_USERNAME is not defined!")
-    if Configs.DB_PASSWORD is None:
-        raise Exception("DB_PASSWORD is not defined!")
+    # if Configs.DB_USERNAME is None:
+    #     raise Exception("DB_USERNAME is not defined!")
+    # if Configs.DB_PASSWORD is None:
+    #     raise Exception("DB_PASSWORD is not defined!")
+    if Configs.DB_CONNECTION_STRING is None:
+        raise Exception("DB_CONNECTION_STRING is not defined!")
     if Configs.DB_NAME is None:
         raise Exception("DB_NAME is not defined!")
     if Configs.DB_COLLECTION_NAME is None:
