313ce6857434a277b90ed62d07b28917b8717024
Riza Semih Koca <rsemihkoca@outlook.com>
2023-06-10 16:26:27 +0300

    commit

--- lib/utils/configs.py
+++ lib/utils/configs.py
@@ -1,8 +1,8 @@
 from lib.utils.helper import get_topic_name
 
-from pydantic import BaseSettings
-
-
+from pydantic import BaseSettings, Field
+import logging
+logger = logging.getLogger()  # Retrieve the root logger
 class Settings(BaseSettings):
     MQTT_USERNAME: str
     MQTT_PASSWORD: str
@@ -17,9 +17,19 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_NAME: str
     DB_COLLECTION_NAME: str
-    DB_CONNECTION_STRING: str = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@clustermik.y0qcdbs.mongodb.net/"
+
     class Config:
         env_file = ".env"
+
+    # MongoDB connection string
+    @property
+    def DB_CONNECTION_STRING(self) -> str:
+        return f"mongodb+srv://{self.DB_USERNAME}:{self.DB_PASSWORD}@clustermik.y0qcdbs.mongodb.net/"
+
+
+    @DB_CONNECTION_STRING.setter
+    def DB_CONNECTION_STRING(self, value):
+        logger.error("DB_CONNECTION_STRING is a read-only property and cannot be modified.")
 
 
 Configs = Settings()
--- lib/utils/mongo_client.py
+++ lib/utils/mongo_client.py
@@ -7,16 +7,17 @@ class MongoDBClient:
     def __init__(self, logger):
         self.logger = logger
         self.client = MongoClient(Configs.DB_CONNECTION_STRING)
-        self.__connnect()
-
         self.db = self.client[Configs.DB_NAME]
         self.collection = self.db[Configs.DB_COLLECTION_NAME]
+        self.__connnect()
+
         self.existing_topics = [str(value) for value in self.collection.distinct(DBConstants.TOPIC)]
 
     def __connnect(self):
         # Send a ping to confirm a successful connection
         try:
-            self.client.admin.command('ping')
+
+            self.db.command('ping')
             self.logger.info("Pinged your deployment. You successfully connected to MongoDB!")
         except Exception as e:
             self.logger.error("Unable to connect to MongoDB Atlas Cluster. Check your MongoDB Atlas cluster settings.")
