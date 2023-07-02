110e3bdb561dee90b803cacc959b87c34ecd03f0
Riza Semih Koca <rsemihkoca@outlook.com>
2023-07-02 23:32:28 +0300

    commit

--- lib/configs/data.py
+++ lib/configs/data.py
@@ -2,4 +2,4 @@ from lib.configs.constants import IDs as TCONS
 
 class TestPayload:
     test_get_status = \
-        {TCONS.Floor01: {TCONS.Desk01: {TCONS.Chair001: False, TCONS.Chair002: False, TCONS.Chair003: False, TCONS.Chair004: False}}}
+        {TCONS.Floor00: {TCONS.Desk01: {TCONS.Chair001: False, TCONS.Chair002: False, TCONS.Chair003: False, TCONS.Chair004: False}}}
