f921c34094eb1f45b8d796e8c48929eb17391c44
Riza Semih Koca <rsemihkoca@outlook.com>
2023-08-12 19:06:01 +0300

    commit

--- main/routers/main_router.py
+++ main/routers/main_router.py
@@ -1,12 +1,6 @@
-from fastapi import APIRouter, Depends
-from starlette.requests import Request
+from fastapi import APIRouter
 from lib.schemas.responses import SUCCESS_RESPONSE_DATA
-# from dependencies import get_current_user
 
-# from lib.utils.responses import success_data_response, handle_exception
-# from typing import Optional
-# from lib.utils.constants import Constants
-# from lib.data_types.base import User
 router = APIRouter()
 
 
